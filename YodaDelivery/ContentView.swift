552195c9567726babf12465a78889db564e89fef
segatt0 <anabellesegatto@hotmail.com>
2022-04-14 16:58:35 -0300

    Initial Commit

---
+++ YodaDelivery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YodaDelivery
+//
+//  Created by Moises Vilas Boas on 14/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YodaDelivery/YodaDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  YodaDeliveryApp.swift
+//  YodaDelivery
+//
+//  Created by Moises Vilas Boas on 14/04/22.
+//
+
+import SwiftUI
+
+@main
+struct YodaDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
