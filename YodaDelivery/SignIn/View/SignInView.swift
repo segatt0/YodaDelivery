ce3a6b867764e69443ca95c115236cec66fee64e
segatt0 <anabellesegatto@hotmail.com>
2022-04-14 20:41:49 -0300

    first steps

--- YodaDelivery/SignIn/View/SignInView.swift
+++ YodaDelivery/SignIn/View/SignInView.swift
@@ -5,4 +5,89 @@
 //  Created by Moises Vilas Boas on 14/04/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct SignInView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        
+        ZStack {
+            Color("yodaTheme")
+            
+                .ignoresSafeArea()
+            ZStack {
+                
+                Color.white
+                    .ignoresSafeArea(edges:.top)
+                    .frame(width: 300, height: 300)
+            }
+            
+        }
+            VStack() {
+                
+                logoYoda
+                fieldLogin
+                Spacer()
+            }
+        }
+    }
+
+
+
+extension SignInView {
+    var logoYoda: some View {
+        VStack(alignment: .center) {
+            Image("Logo")
+                .resizable()
+                .scaledToFit()
+                .padding(.horizontal, 40)
+            
+        }
+    }
+}
+
+extension SignInView {
+    var fieldLogin: some View {
+        VStack(spacing: -15) {
+            VStack(alignment: .leading,spacing: -10) {
+                Text("Login")
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .font(Font.system(size: 25).bold())
+                TextField("  Digite seu email", text: $email)
+                    .frame(height: 50)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                    .foregroundColor(Color.black)
+                
+                    .padding()
+                
+                
+            }
+            VStack(alignment: .leading, spacing: -10) {
+                Text("Password")
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .font(Font.system(size: 25).bold())
+                TextField("Digite sua senha", text: $password)
+                    .foregroundColor(Color.black)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                    .padding()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+struct SignInView_Previws: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
